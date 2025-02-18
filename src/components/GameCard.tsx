28c47711d7187bd8b80d0b84fc07267ad03a9b8a
Hanxmao <hanxmao@hotmail.com>
2025-02-18 00:53:41 -0800

    add no image available icon

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../hooks/useGames";
+import { Game, IconPlatform } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIcons from "./PlatformIcons";
 import CriticScore from "./CriticScore";
@@ -12,12 +12,12 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card>
-        <Image src={getCroppedImageUrl(game.background_image)} />
+        <Image src={getCroppedImageUrl(game.background_image)} height='200px'/>
         <CardBody>
           <Heading fontSize={"xl"}>{game.name}</Heading>
           <HStack justifyContent="space-between">
             <PlatformIcons
-              platforms={game.parent_platforms.map((p) => p.platform)}
+              platforms={game.parent_platforms.map((p) => p.platform as IconPlatform)}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImageAvailable from '../assets/no-image-available.png'
+
 const getCroppedImageUrl = (url:string) => {
-    if(!url) return ''
+    if(!url) return noImageAvailable
     const context = "media/"
     const index = url.indexOf(context) + context.length
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index) 
