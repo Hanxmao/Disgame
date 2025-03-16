77236f959f8187d8e6cf43dd048181ebf569ce06
Hanxmao <hanxmao@hotmail.com>
2025-03-16 01:45:49 -0700

    refactor GameGrid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,20 +1,18 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
-import { GameQuery } from "../App";
+import { Button, SimpleGrid } from "@chakra-ui/react";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Game } from "../services/game-service";
+import { GameQuery } from "../App";
 import { axiosInstance, DataFetched } from "../services/api-client";
+import { Game } from "../services/game-service";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 type Props = {
   gameQuery: GameQuery;
 };
 
 const GameGrid = ({ gameQuery }: Props) => {
-
-  const { isLoading,data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+  const { isLoading, data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery(
       ["games"],
       async ({ pageParam = 1 }) => {
@@ -31,31 +29,43 @@ const GameGrid = ({ gameQuery }: Props) => {
         return res;
       },
       {
-        staleTime: 1000 * 1 * 60, //60 sec
+        staleTime: 1000 * 1 * 60 * 30, //30 mins
         getNextPageParam: (lastPage, allPages) => {
-          return lastPage.data.next? allPages.length+1: null;
+          return lastPage.data.next ? allPages.length + 1 : null;
         },
         keepPreviousData: true,
       }
     );
 
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }} spacing={6}>
-      {isFetchingNextPage||isLoading &&
-        [...Array(12)].map((_, index) => (
-          <GameCardContainer key={index}>
-            <GameCardSkeleton />
-          </GameCardContainer>
-        ))}
-      {data?.pages?.map((page) =>
-        page.data.results.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))
-      )}
-      <Button onClick={()=>{fetchNextPage()}}>Load More</Button>
-    </SimpleGrid>
+    <>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+        spacing={6}
+      >
+        {isLoading &&
+          [...Array(12)].map((_, index) => (
+            <GameCardContainer key={index}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data?.pages?.map((page) =>
+          page.data.results.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game} />
+            </GameCardContainer>
+          ))
+        )}
+      </SimpleGrid>
+      <Button
+        disabled={!hasNextPage}
+        onClick={() => {
+          fetchNextPage();
+        }}
+      >
+        {isFetchingNextPage ? "Loading..." : "Load More"}
+      </Button>
+    </>
   );
 };
 
