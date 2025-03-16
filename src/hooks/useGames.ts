245e8b35bcd0efc9cda2ea591704ab82bf7001fa
Hanxmao <hanxmao@hotmail.com>
2025-03-16 01:33:38 -0700

    refactor useGames hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,24 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import { axiosInstance, DataFetched } from "../services/api-client";
-import { Game } from "../services/game-service";
-
-const getGames = async (gameQuery: GameQuery) => {
-  const res = await axiosInstance.get<DataFetched<Game>>("/games", {
-    params: {
-      genres: gameQuery.genre?.id,
-      parent_platforms: gameQuery.platform?.id,
-      ordering: gameQuery.sort,
-      search: gameQuery.search,
-    },
-  });
-  return res.data;
-};
+import { DataFetched } from "../services/api-client";
+import gameService, { Game } from "../services/game-service";
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<DataFetched<Game>, Error>(
-    ["games", gameQuery], 
-    () => getGames(gameQuery), 
+  useQuery<DataFetched<Game>, Error>(["games", gameQuery], () =>
+    gameService.getAll({
+      params: {
+        genres: gameQuery.genre?.id,
+        parent_platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sort,
+        search: gameQuery.search,
+      },
+    })
   );
 
 export default useGames;
