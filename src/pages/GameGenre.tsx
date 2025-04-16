df7bf4856e2770043d01b78c0b83f243380b6bf6
Hanxmao <hanxmao@hotmail.com>
2025-04-16 14:21:33 -0700

    fix genre games page postion bug

--- src/pages/GameGenre.tsx
+++ src/pages/GameGenre.tsx
@@ -1,14 +1,20 @@
-import { Box } from '@chakra-ui/react'
-import { useParams } from 'react-router'
-import GameGrid from '../components/GameGrid'
+import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import GameGrid from "../components/GameGrid";
 
 const GameGenre = () => {
-    const {genre} = useParams()
-  return (
-    <Box py={{base:6, md:12}}>
-        <GameGrid genre={genre!}/>
-    </Box>
-  )
-}
+  const { genre } = useParams();
 
-export default GameGenre
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Box py={{ base: 6, md: 12 }}>
+      <GameGrid genre={genre!} />
+    </Box>
+  );
+};
+
+export default GameGenre;
